c370919571506fa5a4618fc083b23a2304722807
William0112-AC <aguilarwilli329@gmail.com>
2023-06-28 14:34:54 -0500

    primeroS_TEXTOS

---
+++ android/app/src/main/kotlin/com/example/water_counter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.water_counter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+class StarScreen extends StatefulWidget {
+  const StarScreen({Key.key}) : super(key: key);
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
